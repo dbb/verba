bdaf9a5e32f552d6e8a801a4613e63bae66f6ae8
Daniel Bolton <danielbarrettbolton@gmail.com>
2010-10-11 18:22:26 -0400

    first commit

---
+++ verba.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+use YAML::XS;
+#use Data::Dumper;
+
+open my $fh, '<', 'data.yaml';
+my $yml = do { local $/; <$fh> };
+my $data = Load $yml;
+#print Dumper($config), "\n";
+
+
+#my $test = $data->{TERM}->{PAS}->{SING}->{1}[0];
+my $test = $data->{TERM}->{PAS};
+
+print $test . "\n"; # prints 'r'
+
+my $basis = "am";
+my $thema = $data->{VERBA}->{THEMATA}->{PRAES}->{1};
+my $termi = $data->{VERBA}->{TERM}->{ACT}->{PLUR}->{1};
+my $respo = $basis . $thema . $termi;
+
+print $respo . "\n"; # prints 'amamus'
